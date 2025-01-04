8da5b517359f54f8aa9205fa6f8148f15a8e2a7d
Danilo Gacevic <gacevic.danilo@gmail.com>
2025-01-04 17:02:19 +0100

    fixes

--- android/app/src/main/java/com/stroberi/MainApplication.kt
+++ android/app/src/main/java/com/stroberi/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.nozbe.watermelondb.WatermelonDBPackage; // ⬅️ This!
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -23,8 +24,7 @@ class MainApplication : Application(), ReactApplication {
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
             val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
+            packages.add(WatermelonDBPackage());
             return packages
           }
 
